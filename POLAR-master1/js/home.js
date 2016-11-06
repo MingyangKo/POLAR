32cd8d5dca87ece356e72540593f3574a488536e
MingyangKo <mingyangko@gmail.com>
2016-11-06 06:36:41 -0600

    Add files via upload

---
+++ POLAR-master1/js/home.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    startAt: 1,
+    slideshow: false,
+    controlNav: false,
+    animationLoop: false,
+    keyboard: false,
+  });
+});
+// $(funtion{
+//     $('.biggerleft').mouseenter(function(){
+//     $('.b').css('display','block');
+//   });
+// });
